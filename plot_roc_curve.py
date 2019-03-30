fixed roc curve computation

plot_roc_curve.py
@@ -17,41 +17,24 @@ from sklearn.metrics import roc_curve, auc
 from utils import resample_tractogram
 
 
-def compute_roc_curve(candidate_idx_ranked, true_tract, target_tractogram):
-    """Compute ROC curve. Here candidate_idx_ranked refers to all the candidate 
-       streamlines obtained from multiple examples before refinement.
-    """ 
-    print("Compute the dissimilarity representation of the target tractogram and build the kd-tree.")
-    kdt, prototypes = compute_kdtree_and_dr_tractogram(target_tractogram)
-
-    print("Retrieving indeces of the true_tract")
-    true_tract_idx = streamlines_idx(true_tract, kdt, prototypes)
-
-    print("Compute y_score.")
-    y_score = np.arange(len(candidate_idx_ranked),0,-1)
-
-    #print("Compute y_true.")
-    #diff = np.setdiff1d(true_tract_idx, candidate_idx_ranked)
-
-    #if (len(diff) != 0):
-	#   print("There are %s/%s streamlines of the true tract that aren't in the superset. Making the superset bigger." %(len(diff), len(true_tract_idx)))
-	#   candidate_idx_ranked = np.concatenate([candidate_idx_ranked, diff])
-	#   y_score = np.concatenate([y_score, np.zeros(len(diff))])
-
-    #y_true = np.zeros(len(candidate_idx_ranked))
-    #correspondent_idx_true = np.array([np.where(candidate_idx_ranked==true_tract_idx[i]) for i in range(len(true_tract_idx))])
-    #y_true[correspondent_idx_true] = 1
+def compute_roc_curve(candidate_bundle_idx, min_cost_values, true_tract_idx):
+	"""Compute ROC curve exploiting the cost of the assignment.
+	""" 
+	print("Compute y_score.")
+	idxs = np.unique(candidate_bundle_idx)	
+	how_much_cost = np.array([((candidate_bundle_idx == idx)*min_cost_values).sum() for idx in idxs])
+	y_score = how_much_cost
 
 	print("Compute y_true.")
-	y_true = np.zeros(len(candidate_idx_ranked))
-	correspondent_idx_true = np.hstack([np.where(candidate_idx_ranked==true_tract_idx[i]) for i in range(len(true_tract_idx))])
+	y_true = np.zeros(len(idxs))
+	correspondent_idx_true = np.hstack([np.where(idxs==true_tract_idx[i]) for i in range(len(true_tract_idx))])
 	y_true[correspondent_idx_true] = 1	
 
-    print("Compute ROC curve and AUC.")
-    fpr, tpr, thresholds = roc_curve(y_true, y_score)
-    AUC = auc(fpr, tpr)
+	print("Compute ROC curve and AUC.")
+	fpr, tpr, thresholds = roc_curve(y_true, y_score)
+	AUC = auc(fpr, tpr)
 
-    return fpr, tpr, AUC
+	return fpr, tpr, AUC
 
 
 #def plot_roc_curve(fpr, tpr, AUC, out_fname):
@@ -75,13 +58,13 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-candidate_idx', nargs='?', const=1, default='',
-	                    help='The candidate tract indeces ranked')
+	                    help='The candidate tract indeces')
+	parser.add_argument('-cost', nargs='?',  const=1, default='',
+	                    help='The candidate minimum cost values') 
 	parser.add_argument('-true_tract', nargs='?',  const=1, default='',
 	                    help='The true tract filename')
 	parser.add_argument('-static', nargs='?',  const=1, default='',
 	                    help='The static tractogram filename')  
-	parser.add_argument('-run', nargs='?',  const=1, default='',
-	                    help='The configuration run') 
 	parser.add_argument('-out', nargs='?',  const=1, default='',
 	                    help='The output filename')                               
 	args = parser.parse_args()
@@ -91,7 +74,8 @@ if __name__ == '__main__':
 		step_size = data["step_size"]
 
 	print("Loading data..")
-	candidate_idx_ranked = np.load(args.candidate_idx)
+	candidate_bundle_idx = np.load(args.candidate_idx)
+	min_cost_values = np.load(args.cost)
 	true_tract = nib.streamlines.load(args.true_tract).streamlines
 	print("Resampling with step size = %s mm" %step_size)
 	true_tract_res = resample_tractogram(true_tract, step_size)
@@ -99,8 +83,12 @@ if __name__ == '__main__':
 	static_tractogram = nib.streamlines.load(args.static).streamlines
 	static_tractogram_res = resample_tractogram(static_tractogram, step_size)
 	static_tractogram = static_tractogram_res
+	print("Compute the dissimilarity representation of the target tractogram and build the kd-tree.")
+    kdt, prototypes = compute_kdtree_and_dr_tractogram(static_tractogram)
+    print("Retrieving indeces of the true_tract")
+    true_tract_idx = streamlines_idx(true_tract, kdt, prototypes)
 
-	fpr, tpr, AUC = compute_roc_curve(candidate_idx_ranked, true_tract, static_tractogram)
+	fpr, tpr, AUC = compute_roc_curve(candidate_bundle_idx, min_cost_values, true_tract_idx)
 
 	if args.out:
 		plot_roc_curve(fpr, tpr, AUC, args.out)