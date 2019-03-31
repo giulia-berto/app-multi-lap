1a612f4cb82ed2fb6600ce8256e0a7531c394a6e
Giulia Bertò <giulia.berto.4@gmail.com>
2019-03-31 11:37:15 +0200

    debug mode

--- run.sh
+++ run.sh
@@ -56,81 +56,10 @@ else
 	fi
 fi
 
-echo "Tractogram conversion to trk"
-mkdir tractograms_directory;
-if [[ $static == *.tck ]];then
-	echo "Input in tck format. Convert it to trk."
-	cp $static ./tractogram_static.tck;
-	python tck2trk.py $t1_static tractogram_static.tck -f;
-	cp tractogram_static.trk $subjID'_track.trk';
-	for i in `seq 1 $num_ex`; 
-	do 
-		t1_moving=${arr_t1s[i]//[,\"]}
-		id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
-		cp ${arr_mov[i]//[,\"]} ./${id_mov}_tractogram_moving.tck;
-		python tck2trk.py $t1_moving ${id_mov}_tractogram_moving.tck -f;
-		cp ${id_mov}_tractogram_moving.trk tractograms_directory/$id_mov'_track.trk';
-	done
-fi
 
-if [ -z "$(ls -A -- "tractograms_directory")" ]; then    
-	echo "tractograms_directory is empty."; 
-	exit 1;
-else    
-	echo "tractograms_directory created."; 
-fi
-
-echo "SLR registration"
-for i in `seq 1 $num_ex`; 
-do
-	id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
-	tractogram_moving=tractograms_directory/$id_mov'_track.trk'
-	python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk'
-done
-
-echo "AFQ conversion to trk"
-for i in `seq 1 $num_ex`; 
-do
-	t1_moving=${arr_t1s[i]//[,\"]}
-	id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")		
-	matlab -nosplash -nodisplay -r "afqConverterMulti(${arr_seg[i]//,}, ${arr_t1s[i]//,})";
-	while read tract_name; do
-		echo "Tract name: $tract_name";
-		if [ ! -d "examples_directory_$tract_name" ]; then
-  			mkdir examples_directory_$tract_name;
-		fi
-		mv $tract_name'_tract.trk' examples_directory_$tract_name/$id_mov'_'$tract_name'_tract.trk';
-
-		if [ -z "$(ls -A -- "examples_directory_$tract_name")" ]; then    
-			echo "examples_directory is empty."; 
-			exit 1;
-		else    
-			echo "examples_directory created."; 
-		fi	
-	done < tract_name_list.txt
-done
-echo "AFQ conversion of ground truth to trk"
-matlab -nosplash -nodisplay -r "afqConverter1()";
-
-
-echo "Running multi-LAP"
-mkdir tracts_tck;
 run=multi-LAP	
 
-while read tract_name; do
-	echo "Tract name: $tract_name"; 
-	base_name=$tract_name'_tract'
-	output_filename=tracts_tck/${subjID}_${base_name}_${run}.tck
-	python lap_multiple_examples.py -moving_dir tractograms_directory -static $subjID'_track.trk' -ex_dir examples_directory_$tract_name -out $output_filename;
 
-done < tract_name_list.txt
-
-if [ -z "$(ls -A -- "tracts_tck")" ]; then    
-	echo "multi-LAP failed."
-	exit 1
-else    
-	echo "multi-LAP done."
-fi
 
 echo "Computing ROC curve for multi-LAP"
 mkdir csv;
@@ -148,20 +77,7 @@ echo "Running multi-NN"
 mkdir tracts_tck_nn;
 run=multi-NN	
 
-while read tract_name; do
-	echo "Tract name: $tract_name"; 
-	base_name=$tract_name'_tract'
-	output_filename=tracts_tck_nn/${subjID}_${base_name}_${run}.tck
-	python nn_multiple_examples.py -moving_dir tractograms_directory -static $subjID'_track.trk' -ex_dir examples_directory_$tract_name -out $output_filename;
 
-done < tract_name_list.txt
-
-if [ -z "$(ls -A -- "tracts_tck_nn")" ]; then    
-	echo "multi-NN failed."
-	exit 1
-else    
-	echo "multi-NN done."
-fi
 
 echo "Computing ROC curve for multi-NN"
 while read tract_name; do
@@ -174,26 +90,6 @@ while read tract_name; do
 done < tract_name_list.txt
 
 
-echo "Build partial tractogram"
-run=multi-LAP
-output_filename=${subjID}'_var-partial_tract_'${run}'.tck';
-python build_partial_tractogram.py -tracts_tck_dir 'tracts_tck' -out ${output_filename};
-if [ -f ${output_filename} ]; then 
-    echo "Partial tractogram built."
-else 
-	echo "Partial tractogram missing."
-	exit 1
-fi
 
-echo "Build a wmc structure"
-stat_sub=\'$subjID\'
-tag=\'$run\'
-matlab -nosplash -nodisplay -r "build_wmc_structure($stat_sub, $tag)";
-if [ -f 'output.mat' ]; then 
-    echo "WMC structure created."
-else 
-	echo "WMC structure missing."
-	exit 1
-fi
 
 echo "Complete"
