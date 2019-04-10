168d4249af3adb6d21a2327a771514197bb5b39a
Giulia Berto <gberto@h2.karst.uits.iu.edu>
2019-04-10 12:01:43 -0400

    added possibility to give trk in input

--- run.sh
+++ run.sh
@@ -71,6 +71,14 @@ if [[ $static == *.tck ]];then
 		python tck2trk.py $t1_moving ${id_mov}_tractogram_moving.tck -f;
 		cp ${id_mov}_tractogram_moving.trk tractograms_directory/$id_mov'_track.trk';
 	done
+else
+	echo "Tractogram already in .trk format"
+	cp $static $subjID'_track.trk';
+	for i in `seq 1 $num_ex`; 
+	do 
+		id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
+		cp ${arr_mov[i]//[,\"]} tractograms_directory/$id_mov'_track.trk';
+	done
 fi
 
 if [ -z "$(ls -A -- "tractograms_directory")" ]; then    
@@ -88,34 +96,47 @@ do
 	python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk'
 done
 
-echo "AFQ conversion to trk"
-for i in `seq 1 $num_ex`; 
-do
-	t1_moving=${arr_t1s[i]//[,\"]}
-	id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")		
-	matlab -nosplash -nodisplay -r "afqConverterMulti(${arr_seg[i]//,}, ${arr_t1s[i]//,})";
-	while read tract_name; do
-		echo "Tract name: $tract_name";
-		if [ ! -d "examples_directory_$tract_name" ]; then
-  			mkdir examples_directory_$tract_name;
-		fi
-		mv $tract_name'_tract.trk' examples_directory_$tract_name/$id_mov'_'$tract_name'_tract.trk';
+echo "Tracts conversion to trk"
+if [[ $true_segmentation == *.trk ]];then
+	echo "Tracts already in .trk format"
+	tract_name=$(jq -r "._inputs[2+$num_ex+$num_ex+$num_ex].datatype_tags[1]" config.json | tr -d "_")
+	echo $tract_name > tract_name_list.txt
+	cp $true_segmentation ${tract_name}_tract.trk
+	mkdir examples_directory_$tract_name;
+	for i in `seq 1 $num_ex`; 
+	do
+		id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
+		cp ${arr_mov[i]//[,\"]} examples_directory_$tract_name/$id_mov'_'$tract_name'_tract.trk';
+	done
+else
+	for i in `seq 1 $num_ex`; 
+	do
+		t1_moving=${arr_t1s[i]//[,\"]}
+		id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")		
+		matlab -nosplash -nodisplay -r "afqConverterMulti(${arr_seg[i]//,}, ${arr_t1s[i]//,})";
+		while read tract_name; do
+			echo "Tract name: $tract_name";
+			if [ ! -d "examples_directory_$tract_name" ]; then
+	  			mkdir examples_directory_$tract_name;
+			fi
+			mv $tract_name'_tract.trk' examples_directory_$tract_name/$id_mov'_'$tract_name'_tract.trk';
 
-		if [ -z "$(ls -A -- "examples_directory_$tract_name")" ]; then    
-			echo "examples_directory is empty."; 
-			exit 1;
-		else    
-			echo "examples_directory created."; 
-		fi	
-	done < tract_name_list.txt
+			if [ -z "$(ls -A -- "examples_directory_$tract_name")" ]; then    
+				echo "examples_directory is empty."; 
+				exit 1;
+			else    
+				echo "examples_directory created."; 
+			fi	
+		done < tract_name_list.txt
+	done
+	echo "AFQ conversion of ground truth to trk"
+	matlab -nosplash -nodisplay -r "afqConverter1()";
 done
-echo "AFQ conversion of ground truth to trk"
-matlab -nosplash -nodisplay -r "afqConverter1()";
-
 
 echo "Running multi-LAP"
 mkdir tracts_tck;
-run=multi-LAP
+run=multi-LAP	
+
 while read tract_name; do
 	echo "Tract name: $tract_name"; 
 	base_name=$tract_name'_tract'
@@ -146,6 +167,7 @@ done < tract_name_list.txt
 echo "Running multi-NN"
 mkdir tracts_tck_nn;
 run=multi-NN	
+
 while read tract_name; do
 	echo "Tract name: $tract_name"; 
 	base_name=$tract_name'_tract'
